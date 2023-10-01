bc871311b46937e8c553ed9ef291761bfeb5cd7d
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-10-01 19:10:24 +0200

    first commit

---
+++ src/test/java/com/example/trask_task/TraskTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trask_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraskTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
